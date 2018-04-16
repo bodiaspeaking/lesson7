33653c8e4275177de8f2eb7d7bbda7b384c1749f
bodiaspeaking <bodiagajata@gmail.com>
2018-04-16 21:34:29 +0300

    step2

--- js/js.js
+++ js/js.js
@@ -1,1 +1,34 @@
-alert("Step 2");
+//alert("Step 2");
+
+//Test
+
+//test1
+//small.js
+
+//test2
+//1
+
+var i=3;
+while (i) {
+	console.log (i--);
+}
+
+//test3
+//string,string
+var str = 259;
+var str2 = "string";
+str = str2;
+console.log ('N3 \n',str, str2);
+
+//test4
+//Object
+//Function
+var a = typeof null;
+var b = typeof function () {};
+console.log ('N4 \n', a, '\n', b);
+
+//test5
+//boolean boolean
+var x = true;
+var y = false;
+console.log ('N5 \n', typeof x, '\n', b);
